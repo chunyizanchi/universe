488b3c7523bbb0548a9c547c2b5b72a7a437faf8
chunyizanchi <1021981208@qq.com>
2022-08-13 17:11:30 +0800

    feat：2022-08-13 pom.xml

---
+++ design/src/main/java/com/cyzc/designpattern/singletonPattern/SingletonPattern.java
@@ -0,0 +1,16 @@
+package com.cyzc.designpattern.singletonPattern;
+
+/**
+ * @author: Cyzc
+ * @description:
+ * @create: 2020-12-26
+ */
+public class SingletonPattern {
+    /** The value is used for character storage.
+     *  单例模式：
+     *
+     *
+     * */
+
+
+}
