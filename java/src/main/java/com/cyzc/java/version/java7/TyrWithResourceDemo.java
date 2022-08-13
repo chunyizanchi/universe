0976b491cbf120a3fd2c113cd335a0b6eeacb769
chunyizanchi <1021981208@qq.com>
2022-08-13 12:08:15 +0800

    feat：2022-08-13 try-with-resource

---
+++ java/src/main/java/com/cyzc/java/version/java7/TyrWithResourceDemo.java
@@ -0,0 +1,33 @@
+package com.cyzc.java.version.java7;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+
+/**
+ * <p>
+ *
+ * @author Cyzc
+ * @since [2022/07/25 10:21]
+ */
+public class TyrWithResourceDemo {
+
+    public static void main(String[] args) {
+
+
+     /*   try (FileInputStream fileInputStream=new FileInputStream(
+                "C:\\Users\\10219\\Desktop\\简历_春意暂迟.pdf");
+                int read = fileInputStream.read();
+        ){
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+*/
+
+    }
+}
