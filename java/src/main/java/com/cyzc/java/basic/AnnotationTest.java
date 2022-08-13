12566fd2e0dcaa0adb7edfaf810f0ac8b61f0293
chunyizanchi <1021981208@qq.com>
2022-08-13 16:05:45 +0800

    feat：2022-08-13 idea注释被执行

---
+++ java/src/main/java/com/cyzc/java/basic/AnnotationTest.java
@@ -0,0 +1,18 @@
+package com.cyzc.java.basic;
+
+/**
+ * <p> 注释可以被便于，使用Unicode转换一下。
+ *
+ * @author Cyzc
+ * @since [2022/07/05 15:14]
+ */
+public class AnnotationTest {
+
+    public static void main(String[] args) {
+
+        String name = "你好";
+        //\u000dname="你好啊111111";
+        System.out.println(name);
+    }
+
+}
